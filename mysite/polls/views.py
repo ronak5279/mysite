fa0024866de9fd306d38fa8369a46d52d02fdd3f
ronak5279 <ronak5279.agarwal@gmail.com>
2016-07-15 17:18:51 +0530

    bootstrap and login mech added

---
+++ mysite/polls/views.py
@@ -0,0 +1,139 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+from polls.models import Question,Choice
+from django.utils import timezone
+from django.contrib.auth import authenticate,login,logout
+from mysite import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+	
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+		
+@login_required
+def vote(request,question_id):
+	p=get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice=p.choice_set.get(pk=request.POST['choice'])
+	except(KeyError,Choice.DoesNotExist):
+		return render(request,'polls/detail.html/',{'question':p,'message':"You didn't select a choice."})
+	else:
+		selected_choice.votes+=1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
+	
+def call_signup(request):
+	return render(request,'polls/signup.html/')
+<<<<<<< HEAD
+	
+def create_user(request):
+	username=request.POST.get('username')
+	firstname=request.POST.get('firstname')
+	lastname=request.POST.get('lastname')
+	email=request.POST.get('email')
+	password=request.POST.get('password')
+=======
+def create_user(request):
+	
+	username=request.POST['username']
+	firstname=request.POST['firstname']
+	lastname=request.POST['lastname']
+	email=request.POST['email']
+	password=request.POST['password']
+>>>>>>> 9933ac941a059177e66820fd76340af4d647f51d
+	user=User.objects.create_user(username, email, password)
+	user.first_name=firstname
+	user.last_name=lastname
+	user.save()
+	return HttpResponseRedirect(reverse('polls:index'))
+<<<<<<< HEAD
+	
+def call_login(request):
+	return render(request,'polls/login.html/')
+def verify_user(request):
+	username=request.POST.get('username')
+	password=request.POST.get('password')
+=======
+def call_login(request):
+	return render(request,'polls/login.html/')
+def verify_user(request):
+	username = request.POST['username']
+	password = request.POST['password']
+>>>>>>> 9933ac941a059177e66820fd76340af4d647f51d
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request,user)
+<<<<<<< HEAD
+			return HttpResponseRedirect("success")
+=======
+			return HttpResponseRedirect(reverse('polls:index'))
+>>>>>>> 9933ac941a059177e66820fd76340af4d647f51d
+		else:
+			return render(request,'polls/login.html/',{'message':'The account has been disabled.'})
+	else:
+		return render(request,'polls/login.html/',{'message':'The username-password combination is incorrect.'})
+@login_required
+def call_changepassword(request):
+	return render(request,'polls/changepwd.html/')
+@login_required
+def save_password(request):
+	user=request.user
+	username=request.user.username
+<<<<<<< HEAD
+	oldpassword=request.POST.get('oldpassword')
+	password1=request.POST.get('password1')
+	password2=request.POST.get('password2')
+=======
+	oldpassword=request.POST['oldpassword']
+	password1=request.POST['password1']
+	password2=request.POST['password2']
+>>>>>>> 9933ac941a059177e66820fd76340af4d647f51d
+	test_user= authenticate(username=username, password=oldpassword)
+	if test_user is None:
+		return render(request,'polls/changepassword.html/',{'message':'The existing password is incorrect.'})
+	else:
+		if password1!=password2:
+			return render(request,'polls/changepassword.html/',{'message':'The entered passwords did not match.'})
+		else:
+			if password1:
+				user.set_password(password1)
+				user.save()
+				return render(request,'polls/changepassword.html/',{'message':'The password has been successfully changed.'})
+			else:
+				return render(request,'polls/changepassword.html/',{'message':'Null passwords not accepted.'})
+
+def log_out(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('polls:call_login'))
+
+
